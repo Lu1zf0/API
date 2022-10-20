8a23ae559eb9d635f192e64e2538c182c34e19b9
Lu1zf0 <luizggfo@gmail.com>
2022-10-20 09:05:19 -0300

    Commit pegueporID

--- src/main/java/com/jociel/apicadastro/controller/UsuarioController.java
+++ src/main/java/com/jociel/apicadastro/controller/UsuarioController.java
@@ -2,11 +2,12 @@ package com.jociel.apicadastro.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.jociel.apicadastro.banco.UsuarioBanco;
 import com.jociel.apicadastro.entidade.Usuario;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsuarioController {
@@ -17,6 +18,11 @@ public class UsuarioController {
     @GetMapping("/usuarios")
     public List<Usuario> listaUsuarios(){
         return (List<Usuario>)acessoBanco.findAll();
+    }
+
+    @GetMapping("/usuario{id}")
+    public Optional<Usuario> pegueporID(@PathVariable int id){
+        return acessoBanco.findById(id);
     }
     
 }
